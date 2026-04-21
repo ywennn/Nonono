6db4f8e0c66a928624c0df356236e2b9da5acb7d
ywennn <muhammadrobitzainulhaq@gmail.com>
2026-04-22 03:04:14 +0700

    chore: remove .env from tracking and add to gitignore

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import server from './server/index.js';
-
-const port = process.env.PORT;
-const host = process.env.HOST;
+import 'dotenv/config';
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
 server.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
